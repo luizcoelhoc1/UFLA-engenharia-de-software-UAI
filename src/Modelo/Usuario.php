1d46188cc5063643dcaf8044d768130d33a94044
luizcoelhoc1 <luizcoelhoc1@gmail.com>
2019-06-12 10:43:08 -0300

    Comentários Usuário

--- src/Modelo/Usuario.php
+++ src/Modelo/Usuario.php
@@ -1,10 +1,10 @@
 <?php
 
-/**
- * TODO Auto-generated comment.
- */
 class Usuario {
 
+    /**
+    * Construtor apenas seta os valores dos atributos com os valores passado
+    */
     function __construct($id, $cpf, $email, $nome, $senha = null) {
         $this->id = $id;
         $this->cpf = $cpf;
@@ -12,77 +12,82 @@ class Usuario {
         $this->nome = $nome;
         $this->senha = $senha;
     }
-
-    /**
-     * TODO Auto-generated comment.
-     */
+    
+    //atributos
     private $id;
-
-    /**
-     * TODO Auto-generated comment.
-     */
     private $cpf;
-
-    /**
-     * TODO Auto-generated comment.
-     */
     private $email;
-
-    /**
-     * TODO Auto-generated comment.
-     */
     private $nome;
-
-    /**
-     * TODO Auto-generated comment.
-     */
     private $senha;
 
-    /**
-     * TODO Auto-generated comment.
+    /***
+     * Muda o id do objeto Usuário em questão
      */
-   
     function setId($id) {
         $this->id = $id;
     }
-
+    
+    /***
+     * Muda o CPF do objeto Usuário em questão
+     */
     function setCpf($cpf) {
         $this->cpf = $cpf;
     }
 
+     /***
+     * Muda o Email do objeto Usuário em questão
+     */
     function setEmail($email) {
         $this->email = $email;
     }
-
+    
+     /***
+     * Muda o Nome do objeto Usuário em questão
+     */
     function setNome($nome) {
         $this->nome = $nome;
     }
-
+    
+    /***
+     * Muda a Senha do objeto Usuário em questão
+     */
     function setSenha($senha) {
         $this->senha = $senha;
     }
 
-        
+    /***
+    * Retorna o id do Usuário
+    */
     function getId() {
         return $this->id;
     }
-
+    
+    /***
+    * Retorna o CPF do Usuário
+    */
     function getCpf() {
         return $this->cpf;
     }
-
+    
+    /***
+    * Retorna o email do Usuário
+    */
     function getEmail() {
         return $this->email;
     }
-
+    
+    /***
+    * Retorna o nome do Usuário
+    */
     function getNome() {
         return $this->nome;
     }
-
+    
+    /***
+    * Retorna a senha do Usuário
+    */
     function getSenha() {
         return $this->senha;
     }
-
-
 
 }
