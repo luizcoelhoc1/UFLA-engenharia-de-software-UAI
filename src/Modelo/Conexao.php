bce8ec905b8d8e8f55ae2709ad46c7171eb54fe2
luizcoelhoc1 <luizcoelhoc1@gmail.com>
2019-06-12 10:31:00 -0300

    Comentários no Conexão

--- src/Modelo/Conexao.php
+++ src/Modelo/Conexao.php
@@ -1,24 +1,16 @@
 <?php
 
-/**
- * TODO Auto-generated comment.
- */
 class Conexao {
 
-    /**
-     * TODO Auto-generated comment.
-     */
     public static function open($host, $porta, $bancoDeDados, $usuario, $senha) {
+        //conecta com o banco
         $con = new PDO("mysql:host={$host};port={$porta};dbname={$bancoDeDados}", $usuario, $senha);
 
+        //seta parametros
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $con->Query("SET NAMES utf8");
 
         return $con;
-
-
-
-        return null;
     }
 
 }
