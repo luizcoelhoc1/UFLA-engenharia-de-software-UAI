ba0c2b36ad5d3db8ac5f39a9b3c6d6c89b653d9f
luizcoelhoc1 <luizcoelhoc1@gmail.com>
2019-06-12 11:00:09 -0300

    Comentários em Template

--- src/Visualizacao/Template.php
+++ src/Visualizacao/Template.php
@@ -1,36 +1,33 @@
 <?php
 
-/**
- * TODO Auto-generated comment.
- */
 class Template {
 
-    /**
-     * TODO Auto-generated comment.
+    /***
+     * Valores cuja chave será a chave de mudança do template e o valor o valor q será substituido
      */
     private $valores;
 
-    /**
-     * TODO Auto-generated comment.
+    /***
+     * O arquivo do template
      */
     private $arquivo;
 
-    /**
-     * TODO Auto-generated comment.
+    /***
+     * Como será identificado o começo chave
      */
     public static $comecoChave = "[@";
 
-    /**
-     * TODO Auto-generated comment.
+    /***
+     * Como será identificado o final chave
      */
     public static $fimChave = "]";
 
-    /**
-     * TODO Auto-generated comment.
+    
+    /***
+     * Construtor tenta verificar se o arquivo passado é um endereço de um arquivo, se for lê o arquivo e o trata como o template, caso contrário trata a string passada por parametro como o template
      */
     public function __construct($arquivo, $chave = null) {
         $valores = array();
-
         if (file_exists($arquivo)) {
             $this->arquivo = file_get_contents($arquivo);
         } else {
@@ -41,12 +38,15 @@ class Template {
         }
     }
 
+    /***
+    * Seta uma chave do template com um valor
+    */
     public function set($chave, $valor) {
         $this->valores[$chave] = $valor;
     }
 
-    /**
-     * TODO Auto-generated comment.
+    /***
+     * Retorna o template montado
      */
     public function output() {
         if (!empty($this->valores)) {
